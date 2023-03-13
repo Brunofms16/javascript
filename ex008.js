403fd69b835caac538ca26ad110a61acf46ba29b
Brunofms16 <122386476+Brunofms16@users.noreply.github.com>
2023-03-13 11:16:02 -0300

    main

---
+++ ex008.js
@@ -0,0 +1,11 @@
+var vel = 99.5 
+
+console.log(`a velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60 ){
+    // condição simples
+    console.log("voce ultrapassou a velocidade permitida : MULTADO!")
+}
+else {
+    console.log("dirija sempre com segurança!")
+}
+
---
+++ ex009.js
@@ -0,0 +1,8 @@
+var pais = "B99"
+
+if(pais != "BR") {
+    console.log("voce é estrangeiro")
+}
+else {
+    console.log("voce é brasileiro")
+}
